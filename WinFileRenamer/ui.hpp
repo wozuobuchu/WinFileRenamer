2edb09eda7d747b0da8b6307a88f6d9e746f09f7
even <enjoy.Minecraft@outlook.com>
2025-11-14 01:24:49 +0800

    update

--- WinFileRenamer/ui.hpp
+++ WinFileRenamer/ui.hpp
@@ -28,6 +28,9 @@
 // Link the common controls library
 #pragma comment(lib, "Comctl32.lib")
 
+#pragma comment(linker,"\"/manifestdependency:type='win32' \
+name='Microsoft.Windows.Common-Controls' version='6.0.0.0' \
+processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
 
 namespace ui {
 
@@ -327,7 +330,7 @@ namespace ui {
 		// Initialize common controls
 		INITCOMMONCONTROLSEX icex;
 		icex.dwSize = sizeof(INITCOMMONCONTROLSEX);
-		icex.dwICC = ICC_LISTVIEW_CLASSES; // Register the list view class
+		icex.dwICC = ICC_LISTVIEW_CLASSES | ICC_STANDARD_CLASSES;
 		InitCommonControlsEx(&icex);
 
 
