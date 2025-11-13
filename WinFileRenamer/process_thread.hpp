4c91d4da48079117485d4503a8caf5135842811b
even <enjoy.Minecraft@outlook.com>
2025-11-13 09:35:12 +0800

    processing

--- WinFileRenamer/process_thread.hpp
+++ WinFileRenamer/process_thread.hpp
@@ -10,6 +10,7 @@
 #include <exception>
 #include <sstream>
 #include <cstdint>
+#include <filesystem>
 
 namespace pt {
 
@@ -46,26 +47,66 @@ private:
 
 		calc::var_index = 0;
 
-		bool calc_flag = true;
+		bool calc_flag = false;
 		try {
 			std::shared_lock<std::shared_mutex> lck(input_expr_ptr_smtx);
 			auto rpn_ptr = calc::generate_rpn(input_expr_ptr);
 			for (const auto& filepath : vec_filepath) vec_newname.emplace_back(calc::calculate_rpn(calc::preprocess_rpn(rpn_ptr)));
-		} catch (std::runtime_error& re) {
+			calc_flag = true;
+		} catch (const std::runtime_error& re) {
 			std::wstringstream wss;
 			wss << re.what();
 			{
 				std::unique_lock<std::shared_mutex> lck(res_wstr_smtx);
 				res_wstr = wss.str();
 			}
-			calc_flag = false;
 		}
 
-		bool rename_flag = true;
+		bool rename_flag = false;
+		size_t vsize = vec_filepath.size();
+		size_t success_cnt = 0;
 		if (calc_flag) {
-
+			try {
+				size_t vsize = vec_filepath.size();
+				for (size_t i = 0; i < vsize; ++i) {
+					if (!std::filesystem::exists(vec_filepath[i])) throw std::runtime_error("File doesn't exist !");
+					if (std::filesystem::exists(vec_newname[i])) throw std::runtime_error("Target file already exists !");
+					std::filesystem::rename(vec_filepath[i], vec_newname[i]);
+					++success_cnt;
+				}
+				rename_flag = true;
+			} catch (const std::filesystem::filesystem_error& e) {
+				std::wstringstream wss;
+				wss << e.what();
+				{
+					std::unique_lock<std::shared_mutex> lck(res_wstr_smtx);
+					res_wstr = wss.str();
+				}
+			} catch (const std::runtime_error& re) {
+				std::wstringstream wss;
+				wss << re.what();
+				{
+					std::unique_lock<std::shared_mutex> lck(res_wstr_smtx);
+					res_wstr = wss.str();
+				}
+			} catch (...) {
+				std::wstringstream wss;
+				wss << "Unknown Error !";
+				{
+					std::unique_lock<std::shared_mutex> lck(res_wstr_smtx);
+					res_wstr = wss.str();
+				}
+			}
 		}
 
+		if (rename_flag && calc_flag) {
+			std::wstringstream wss;
+			wss << L"Successfully renamed " << success_cnt << L" / " << vsize << L" files.";
+			{
+				std::unique_lock<std::shared_mutex> lck(res_wstr_smtx);
+				res_wstr = wss.str();
+			}
+		}
 
 		state_.store(STATE_READY);
 		msg_box_.store(true);
@@ -73,8 +114,11 @@ private:
 
 public:
 
-	void process_lunch() {
-
+	bool process_lunch() {
+		if (state_.load() == STATE_ONGOING) return false;
+		if(rename_thread.joinable()) rename_thread.join();
+		rename_thread = std::thread(std::bind(&ProcessThread::rename_thread_assist,this));
+		return true;
 	}
 
 	ProcessThread() {
@@ -89,10 +133,6 @@ public:
 	}
 
 	void reset_selected_file() {
-		{
-			std::unique_lock<std::shared_mutex> lck(res_wstr_smtx);
-			res_wstr.clear();
-		}
 		{
 			std::unique_lock<std::shared_mutex> lck(vec_filepath_cache_smtx);
 			vec_filepath_cache.clear();
